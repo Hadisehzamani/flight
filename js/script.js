9f6e264571e9a3b93f0b2f532da70cdb4a183786
Hadisehzamani <hadisehzmn@gmail.com>
2023-06-28 15:53:46 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿let dataCities = {
+    Iran: ['tehran','ardebil','tabriz','kermanshah'],
+    Canada: ['vancover','torento'],
+    Turkey: ['adana','ghazi antep','estanbol','antalia']
+}
+
+let citySelect = document.querySelector('.citySelect')
+
+let selectBox = document.querySelector('.countrySelect')
+
+selectBox.addEventListener('change', function(){
+   let selectValue = selectBox.value
+   
+   citySelect.innerHTML = "";
+   dataCities[selectValue].forEach(element => {
+    citySelect.innerHTML += `<option>${element}</option>`
+   })
+})
